1162ab4305b8e5ec0a58d989a9eb164f87c7c23b
Nonya Biz Ness <164905463+linuxfandudeguy@users.noreply.github.com>
2024-07-20 21:33:26 -0400

    Create base64.js

---
+++ base64.js
@@ -0,0 +1,11 @@
+var encode = "Hello, World!"
+
+var decode = "SGVsbG8sIFdvcmxkIQ=="
+
+console.log("The decoded result is:")
+console.log(atob(decode))
+console.log("             ")
+console.log("-----------------------")
+console.log("             ")
+console.log("The encoded result is:")
+console.log(btoa(encode))
